9840b0af2095ac38f6e1df125dfa7662bab5d718
MajdSoubh <majdsoubh53@gmail.com>
2025-02-07 13:31:54 +0300

    [REFACTOR] updated auth routes

--- backend/routes/auth.php
+++ backend/routes/auth.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
+use App\Http\Controllers\PasswordResetController;
+use App\Http\Controllers\VerificationController;
 
 // Admin Auth Routes
 Route::group([
@@ -15,8 +16,6 @@ Route::group([
     Route::middleware(['guest:sanctum,admin'])->group(function ()
     {
         Route::post('/login', 'login')->name('login');
-        Route::post('/forget-password', 'forgetPassword')->name('password.forget');
-        Route::post('/reset-password/{token?}', 'resetPassword')->name('password.reset');
     });
     Route::middleware(['auth:sanctum,admin'])->group(function ()
     {
@@ -36,8 +35,6 @@ Route::group([
     {
         Route::post('/login', 'login')->name('login');
         Route::post('/register', 'register')->name('store');
-        Route::post('/forget-password', 'forgetPassword')->name('password.forget');
-        Route::post('/reset-password/{token?}', 'resetPassword')->name('password.reset');
     });
     Route::middleware(['auth:sanctum,client'])->group(function ()
     {
@@ -45,3 +42,25 @@ Route::group([
         Route::get('/logout', 'logout')->name('logout');
     });
 });
+
+// Password Reset Routes
+Route::group([
+    'controller' => PasswordResetController::class,
+    'middleware' => 'throttle:10,1'
+], function ()
+{
+    Route::middleware('guest:sanctum,client')->group(function ()
+    {
+        Route::post('/forget-password', 'forgetPassword')->name('password.forget');
+        Route::post('/reset-password/{token?}', 'resetPassword')->name('password.reset');
+    });
+});
+
+// Email Verification Routes
+Route::get('/email/verify/{id}/{hash}', [VerificationController::class, 'verifyEmail'])
+    ->middleware(['signed', 'throttle:6,1'])
+    ->name('verification.verify');
+
+Route::post('/email/resend', [VerificationController::class, 'resendVerificationEmail'])
+    ->middleware(['auth:sanctum,client,admin', 'throttle:6,1'])
+    ->name('verification.resend');
